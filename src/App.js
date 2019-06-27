deff3e33b9f86f4d1785672bcc03d8b5c1662fcc
Jack Maddox <jackdmaddox@gmail.com>
2019-06-26 21:13:17 -0400

    it actually works! Corrected a capitalisation error and added unflip functionality

--- src/App.js
+++ src/App.js
@@ -2,7 +2,93 @@ import React from 'react';
 import './App.css';
 import MemoryCard from './MemoryCard.js'
 
-function App() {
+function generateDeck() {
+  const symbols = ["∆", "@", "$", "%", "8", "&", "ø", "π"];
+  let deck =[];
+  for (let i=0; i < 16; i++) {
+    deck.push({
+      symbol: symbols[i%8],
+      isFlipped: false
+    });
+  } ;
+  shuffle(deck);
+  console.log('this is the deck', deck);
+  return deck;
+}
+
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = a[i];
+      a[i] = a[j];
+      a[j] = x;
+  }
+  return a;
+}
+
+class App extends React.Component {
+  state = {
+    deck: generateDeck(),
+    pickedCards: []
+  }
+
+  pickCard(cardIndex) {
+    if (this.state.deck[cardIndex].isFlipped)
+    return;
+
+    let cardToFlip = {...this.state.deck[cardIndex]}
+    cardToFlip.isFlipped = true;
+    let newPickedCards = this.state.pickedCards.concat(cardIndex)
+    let newDeck = this.state.deck.map((card, index) => {
+      //THIS NEEDS MOVING TO THE TOP
+      if (cardIndex === index){
+        return cardToFlip;
+      }
+      return card;
+    });
+    if (newPickedCards.length === 2){
+      let card1Index = newPickedCards[0];
+      let card2Index = newPickedCards[1];
+      let card1 = newDeck[card1Index];
+      let card2 = newDeck[card2Index];
+      if (card1.symbol !== card2.symbol){
+        setTimeout(()=>{
+          this.unflipCards(card1Index, card2Index);
+
+        }, 1000);
+      }
+      newPickedCards = [];
+    }
+    this.setState({deck: newDeck, pickedCards: newPickedCards});
+  }
+
+  unflipCards(card1Index, card2Index) {
+    let newDeck = this.state.deck.map(card => {
+      return {...card}
+    });
+
+    newDeck[card1Index].isFlipped = false;
+    newDeck[card2Index].isFlipped = false;
+
+    this.setState({
+      deck:newDeck
+    });
+  
+  }
+  //RESUME HERE^^
+
+  render() {
+
+    let cardsJSX = this.state.deck.map((card, index) => {
+      return <MemoryCard 
+      symbol= {card.symbol} 
+      isFlipped = {card.isFlipped}
+      key = {index}
+      pickCard = {this.pickCard.bind(this,index)}
+          />
+    }) ;
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,31 +96,20 @@ function App() {
         <h3>Match cards to win</h3>
       </header>
       <div className="row1">
-        <MemoryCard/>
-        <MemoryCard/>
-        <MemoryCard/>
-        <MemoryCard/>
+        {cardsJSX.slice(0,4)}        
       </div>
       <div className="row2">
-        <MemoryCard/>
-        <MemoryCard/>
-        <MemoryCard/>
-        <MemoryCard/>
+        {cardsJSX.slice(4,8)}        
       </div>
       <div className="row3">
-        <MemoryCard/>
-        <MemoryCard/>
-        <MemoryCard/>
-        <MemoryCard/>
+        {cardsJSX.slice(8,12)}
       </div>
       <div className="row4">
-        <MemoryCard/>
-        <MemoryCard/>
-        <MemoryCard/>
-        <MemoryCard/>
+        {cardsJSX.slice(12,16)}
       </div>
     </div>
   );
+}
 }
 
 export default App;
--- src/MemoryCard.js
+++ src/MemoryCard.js
@@ -6,25 +6,25 @@ state = {
     isFlipped: false
 }
 
-    clickHandler = (e) => {
-        e.preventDefault();
-        this.setState({
-            isFlipped: true
-        });
-        console.log(this.state.isFlipped);
-    }
+    // clickHandler = (e) => {
+    //     e.preventDefault();
+    //     this.setState({
+    //         isFlipped: true
+    //     });
+    //     console.log(this.state.isFlipped);
+    // }
 
     render() {
         let memoryCardInnerClass = "MemoryCardInner"
-        if (this.state.isFlipped === true) {
+        if (this.props.isFlipped === true) {
             memoryCardInnerClass = memoryCardInnerClass + " flipped"
             console.log(memoryCardInnerClass);
         }
         return (
-            <div className="MemoryCard" onClick={(e) => this.clickHandler(e)}>
+            <div className="MemoryCard" onClick={this.props.pickCard}>
                 <div className={memoryCardInnerClass}>
                     <div className='MemoryCardBack'><img src="https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png"></img></div>
-                    <div className="MemoryCardFront">∆</div>
+                    <div className="MemoryCardFront">{this.props.symbol}</div>
                 </div>
             </div>
         );
